3a95bed57b890ef49f20c0392be22fdaaa1eed73
jdsee <joscha.seelig@linked-planet.com>
2025-09-10 08:51:01 +0200

    Cleanup Git Aliases

--- home/cli/git/default.nix
+++ home/cli/git/default.nix
@@ -9,44 +9,54 @@
       # prepare-commit-msg = ./hooks/prepare-commit-msg;
     };
     aliases = {
-      last = "log -n 1 --stat";
+      # general
       aa = "add --all";
+      s = "status";
+      sw = "switch";
+      mt = "mergetool";
+      unstage = "reset HEAD - -";
+      dc = "diff --cached";
+      alias = "!git config - l | grep ^alias | cut - c 7 - | sort";
+
+      # push
+      p = "push";
+      fp = "push --force";
+      fl = "push --force-with-lease";
+      pwt = "push --atomic origin"; # Push branch and tag simultaneously: git pwt <branch> <tag>
+
+      # commit
+      c = "commit";
+      cane = "commit --amend --no-edit";
+      wip = "!git commit -m \"[WIP] $(curl -s https://whatthecommit.com/index.txt)\"";
+
+      # rebase
       rb = "rebase";
       rba = "rebase --abort";
       rbc = "rebase --coninue";
       rbs = "rebase --skip";
       rbi = "rebase --interactive";
+
+      # cherry-pick
       cp = "cherry-pick";
       co = "checkout";
       cl = "clone";
-      mt = "mergetool";
-      c = "commit";
-      cane = "commit --amend --no-edit";
-      s = "status";
-      sw = "switch";
-      fp = "push --force";
-      fl = "push --force-with-lease";
+
+      # branch
       br = "branch";
+
+      # worktree
       wt = "worktree";
-      unstage = "reset HEAD - -";
-      dc = "diff --cached";
+      clone-multibranch = "!sh ~/.config/git/clone_bare_for_worktrees.sh";
+
+      # log
       l = "log --oneline";
       lh = "log --oneline -n 15";
+      last = "log -n 1 --stat";
       lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
       res = "!git fetch && git reset origin/`git rev-parse --abbrev-ref HEAD` --hard";
       graph = ''log --graph --color --pretty=format:"%C (yellow)%H%C (green)%d%C (reset)%n%x20%cd%n%x20%cn%x20 (%ce)%n%x20%s%n "'';
-      tags = "tag - l"; # Show verbose output about tags, branches or remotes;
-      branches = "branch - a";
-      remotes = "remote - v";
-      amend = "commit --amend --reuse-message=HEAD"; # Amend the currently staged files to the latest commit;
-      contributors = "shortlog --summary --numbered"; # List contributors with number of commits;
-      alias = "!git config - l | grep ^alias | cut - c 7 - | sort";
-      ir = "rebase -i --autostash";
-      pwt = "push --atomic origin"; # Push branch and tag simultaneously: git pwt <branch> <tag>
-      ps = "pull --autostash";
-      conflicts = "!grep -lr '<<<<<<<' .";
-      clone-multibranch = "!sh ~/.config/git/clone_bare_for_worktrees.sh";
-      rebase-last = "!git rebase --interactive --autostash HEAD~$1";
+
+      # tag
       inc-patch = ''
         !git tag --list --sort v:refname | tail -n 1 | awk -F. '{ print $1 "." $2 "." ($3 + 1) }' | xargs -p -I _ sh -c 'git tag _ -m _ && git push && git push origin _'
       '';
@@ -56,7 +66,13 @@
       inc-major = ''
         !git tag --list --sort v:refname | tail -n 1 | awk -F. '{ print ($1 + 1) ".0.0" }' | xargs -p -I _ sh -c 'git tag _ -m _ && git push && git push origin _'
       '';
-      wip = "!git commit -m \"$(curl -s https://whatthecommit.com/index.txt)\"";
+
+      # list
+      branches = "branch - a";
+      remotes = "remote - v";
+      tags = "tag - l"; # Show verbose output about tags, branches or remotes;
+      conflicts = "!grep -lr '<<<<<<<' .";
+      contributors = "shortlog --summary --numbered"; # List contributors with number of commits;
     };
     extraConfig = {
       core.editor = "nvim";
